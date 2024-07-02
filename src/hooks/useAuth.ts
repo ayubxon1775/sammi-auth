185d9c7991b7487dd3929133ef75d20c2af61110
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-07-02 17:42:18 +0500

    added user and password loading

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,45 @@
-import { User } from "firebase/auth";
+import { User, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react"
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const useAuth = () => {
   const [isloading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
   const [user, setUser] = useState<User>({} as User);
+  const navigate = useNavigate();
+
+  const signUp = async (email: string, password: string) => {
+    setIsLoading(true);
+  await createUserWithEmailAndPassword(auth, email, password).then(res => {
+    setUser(res.user);
+    setIsLoading(false)
+    navigate('/')
+  }).catch(error => {
+    const result = error as Error;
+    setError(result.message);
+  })
+  .finally(() => setIsLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setIsLoading(true);
+    await signInWithEmailAndPassword(auth, email, password).then(res => {
+      setUser(res.user);
+      setIsLoading(false)
+      navigate('/')
+    }).catch(error => {
+      const result = error as Error;
+      setError(result.message);
+    })
+    .finally(() => setIsLoading(false));
+  };
+
+  const logout = () => {
+    
+  };
+
+  return { signIn, signUp, logout,  isloading, error }
 };
 
 export default useAuth
--- src/page/auth.tsx
+++ src/page/auth.tsx
@@ -1,30 +1,60 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const auth = () => {
-    const [auth, setAuth] = useState< 'signup' | 'signin' >('signin')
+    const [auth, setAuth] = useState< 'signup' | 'signin' >('signin');
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [invalid, setInvalid] = useState<boolean>(false)
+    const { signUp, signIn, error, isloading} = useAuth()
 
     const toggleAuth = (state:'signup' | 'signin') => {
         setAuth(state)
     };
 
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if(!password.length || !email.length){
+        setInvalid(true);
+      }
+      setInvalid(false);
+      if (auth === 'signup'){
+        signUp(email, password);
+      }else {
+        signIn(email, password);
+      }
+    };
+
   return (
   <main className=" container form-signin text-center mt-4">
-  <form className="m-auto w-50">
+  <form className="m-auto w-50" onSubmit={onSubmit}>
     <img className="mb-4" src="https://media.graphassets.com/BldVa3tyRDy2QkoyQq9R" alt="" width="72" height="57"/>
     <h1 className="h3 mb-3 fw-normal text-start">{auth =='signup' ? 'Sign up' : 'Sign in'}</h1>
-
+    {error && <div className="alert alert-danger">{error}</div>}
     <div className="form-floating">
-      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+      <input 
+      type="email" 
+      onChange={e => setEmail(e.target.value)}
+      value={email}
+       className={`form-control ${invalid && 'is-invalid'} `} 
+       id="floatingInput" 
+       placeholder="name@example.com"
+       />
       <label htmlFor="floatingInput">Email address</label>
     </div>
     <div className="form-floating mt-2">
-      <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+      <input type="password"
+      onChange={e => setPassword(e.target.value)}
+      value={password}
+      
+       className={`form-control ${invalid && 'is-invalid'} `} 
+        id="floatingPassword" placeholder="Password"/>
       <label htmlFor="floatingPassword">Password</label>
     </div>
 
     
-    <button className="btn btn-lg btn-primary mt-2 w-100 py-2" type="submit">
-      {auth === 'signup' ? 'Sign Up' : 'Sign in'}</button>
+    <button className="btn btn-lg btn-primary mt-2 w-100 py-2" disabled={isloading} type="submit">
+      {isloading ? 'Loading...' : auth === 'signup' ? 'Sign Up' : 'Sign in'}</button>
   
     <p className="mt-2 fw-bold">
       {auth === 'signup' ? 'Already have account': 'Not account yet'} {' '}
